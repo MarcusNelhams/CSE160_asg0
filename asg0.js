2d5150beae1a91e17c3ba4e396e27fe19722af16
Marcus Nelhams <114110016+MarcusNelhams@users.noreply.github.com>
2024-04-05 16:48:10 -0700

    Add files via upload

---
+++ asg0.js
@@ -0,0 +1,76 @@
+// HelloPoint1.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  void main() {
+    gl_Position = a_Position;
+    gl_PointSize = 10.0;
+  }`
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+  precission mediump float;
+  uniform vec4 u_FragColor;
+  void main() {
+    gl_FragColor = u_FragColor;
+  }`
+
+// Global Vars
+let canvas;
+let gl;
+let a_Position;
+let u_FragColor;
+
+// Sets up WebGL
+function setuptWebGL() {
+  // Retrieve <canvas> element
+  canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+}
+
+// Connects js vars to GLSL vars in shaders
+function connectVarsToGLSL() {
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Get the storage location of a_Position
+  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+
+  // Get the storage location of u_FragColor
+  u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  if (!u_FragColor) {
+    console.log('Failed to get the storage location of u_FragColor');
+    return;
+  }
+}
+
+function main() {
+
+  setupWebGL();
+  connectVarsToGLSL();
+
+  // Pass vertex position to attribute variable
+  gl.vertexAttrib3f(a_Position, 0.5, 0.0, 0.0);
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw a point
+  gl.drawArrays(gl.POINTS, 0, 1);
+}
