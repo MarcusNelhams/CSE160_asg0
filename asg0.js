51c0c85697f55622931f1bcdd26c7b6e8beaf0a2
Marcus Nelhams <114110016+MarcusNelhams@users.noreply.github.com>
2024-04-08 15:22:33 -0700

    Add files via upload

--- asg0.js
+++ asg0.js
@@ -1,76 +1,90 @@
-// HelloPoint1.js (c) 2012 matsuda
-// Vertex shader program
-var VSHADER_SOURCE = `
-  attribute vec4 a_Position;
-  void main() {
-    gl_Position = a_Position;
-    gl_PointSize = 10.0;
-  }`
-
-// Fragment shader program
-var FSHADER_SOURCE = `
-  precission mediump float;
-  uniform vec4 u_FragColor;
-  void main() {
-    gl_FragColor = u_FragColor;
-  }`
-
-// Global Vars
+// DrawRectangle.js
 let canvas;
-let gl;
-let a_Position;
-let u_FragColor;
+let ctx;
 
-// Sets up WebGL
-function setuptWebGL() {
-  // Retrieve <canvas> element
-  canvas = document.getElementById('webgl');
+function canvasSetUp() {
+  // Retrieve <canvas> element <- (1)
+  canvas = document.getElementById('example');
+  if (!canvas) {
+    console.log('Failed to retrieve the <canvas> element');
+    return;
+  }
 
-  // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
+  // Get the rendering context for 2DCG <- (2)
+  ctx = canvas.getContext('2d');
+  if (!ctx) {
+    console.log('Failed to retrieve the context element');
     return;
   }
 }
 
-// Connects js vars to GLSL vars in shaders
-function connectVarsToGLSL() {
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
+function drawVector(v, color) {
+  var x = v.elements[0];
+  var y = v.elements[1];
+  ctx.beginPath();
+  ctx.moveTo(200, 200);
+  ctx.lineTo(200 + x*20, 200 - y*20);
+  ctx.strokeStyle = color;
+  ctx.stroke();
+  return;
+}
+
+function handleDrawEvent() {
+  
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // set a black canvas color
+  ctx.fillRect(0, 0, canvas.width, canvas.height); // fill the canvas
+
+  // get input values
+  var v1xInput = document.getElementById("v1xInput");
+  if (!v1xInput) {
+    console.log('unaible to get v1x input');
+    return;
+  }
+  var v1yInput = document.getElementById("v1yInput");
+  if (!v1yInput) {
+    console.log('unaible to get v2y input');
     return;
   }
 
-  // Get the storage location of a_Position
-  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
+  v1xValue = v1xInput.value;
+  v1yValue = v1yInput.value;
+
+  // draw v1 vector
+  let v1 = new Vector3([v1xValue, v1yValue, 0]);
+  drawVector(v1, 'red');
+
+  var v2xInput = document.getElementById("v2xInput");
+  if (!v2xInput) {
+    console.log("unable to get v2x input");
     return;
   }
 
-  // Get the storage location of u_FragColor
-  u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-  if (!u_FragColor) {
-    console.log('Failed to get the storage location of u_FragColor');
+  var v2yInput = document.getElementById("v2yInput");
+  if (!v2yInput) {
+    console.log("unable to get v2y input");
     return;
   }
+
+  v2xValue = v2xInput.value;
+  v2yValue = v2yInput.value;
+
+  // draw v2 vector
+  let v2 = new Vector3([v2xValue, v2yValue, 0]);
+  drawVector(v2, 'blue');
+  return;
 }
 
 function main() {
+  // setup
+  canvasSetUp();
 
-  setupWebGL();
-  connectVarsToGLSL();
+  // Draw the canvas
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // set a black canvas color
+  ctx.fillRect(0, 0, canvas.width, canvas.height); // fill the canvas
 
-  // Pass vertex position to attribute variable
-  gl.vertexAttrib3f(a_Position, 0.5, 0.0, 0.0);
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Draw a point
-  gl.drawArrays(gl.POINTS, 0, 1);
-}
+  // button set up
+  var button = document.getElementById("button");
+  button.addEventListener("click", handleDrawEvent);
+} 
