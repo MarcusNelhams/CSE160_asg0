4800cea9c6f115e75cd768cb1ae448b08f77392d
Marcus Nelhams <114110016+MarcusNelhams@users.noreply.github.com>
2024-04-08 17:01:51 -0700

    Add files via upload

--- asg0.js
+++ asg0.js
@@ -36,7 +36,7 @@ function handleDrawEvent() {
   ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // set a black canvas color
   ctx.fillRect(0, 0, canvas.width, canvas.height); // fill the canvas
 
-  // get input values
+  // get v1 input values
   var v1xInput = document.getElementById("v1xInput");
   if (!v1xInput) {
     console.log('unaible to get v1x input');
@@ -55,6 +55,7 @@ function handleDrawEvent() {
   let v1 = new Vector3([v1xValue, v1yValue, 0]);
   drawVector(v1, 'red');
 
+  // get v2 input values
   var v2xInput = document.getElementById("v2xInput");
   if (!v2xInput) {
     console.log("unable to get v2x input");
@@ -73,6 +74,82 @@ function handleDrawEvent() {
   // draw v2 vector
   let v2 = new Vector3([v2xValue, v2yValue, 0]);
   drawVector(v2, 'blue');
+}
+
+function handleDrawOperationEvent() {
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // set a black canvas color
+  ctx.fillRect(0, 0, canvas.width, canvas.height); // fill the canvas
+
+  // get v1 input values
+  var v1xInput = document.getElementById("v1xInput");
+  if (!v1xInput) {
+    console.log('unaible to get v1x input');
+    return;
+  }
+  var v1yInput = document.getElementById("v1yInput");
+  if (!v1yInput) {
+    console.log('unaible to get v2y input');
+    return;
+  }
+
+  v1xValue = v1xInput.value;
+  v1yValue = v1yInput.value;
+
+  // draw v1 vector
+  let v1 = new Vector3([v1xValue, v1yValue, 0]);
+  drawVector(v1, 'red');
+
+  // get v2 input values
+  var v2xInput = document.getElementById("v2xInput");
+  if (!v2xInput) {
+    console.log("unable to get v2x input");
+    return;
+  }
+
+  var v2yInput = document.getElementById("v2yInput");
+  if (!v2yInput) {
+    console.log("unable to get v2y input");
+    return;
+  }
+
+  v2xValue = v2xInput.value;
+  v2yValue = v2yInput.value;
+
+  // draw v2 vector
+  let v2 = new Vector3([v2xValue, v2yValue, 0]);
+  drawVector(v2, 'blue');
+
+  // get operation
+  var opSelect = document.getElementById('op');
+  var op = opSelect.value;
+  console.log(op);
+
+  var scalObj = document.getElementById('Scalar');
+  var scal = scalObj.value;
+
+  var v3;
+  var v4;
+
+  if (op == 'add') {
+    v3 = v1.add(v2);
+    drawVector(v3, 'green');
+  } else if (op == 'sub') {
+    v3 = v1.sub(v2);
+    drawVector(v3, 'green');
+  } else if (op == 'mul') {
+    v3 = v1.mul(scal);
+    v4 = v2.mul(scal);
+    drawVector(v3, 'green');
+    drawVector(v4, 'green');
+  } else if (op == 'div') {
+    v3 = v1.div(scal);
+    v4 = v2.div(scal);
+    drawVector(v3, 'green');
+    drawVector(v4, 'green');
+  }
+
   return;
 }
 
@@ -85,6 +162,9 @@ function main() {
   ctx.fillRect(0, 0, canvas.width, canvas.height); // fill the canvas
 
   // button set up
-  var button = document.getElementById("button");
-  button.addEventListener("click", handleDrawEvent);
+  var drawButton = document.getElementById("drawButton");
+  drawButton.addEventListener("click", handleDrawEvent);
+
+  var opButton = document.getElementById('opButton');
+  opButton.addEventListener("click", handleDrawOperationEvent);
 } 
